d37220d8a62f5806140049891d21c36aa2232930
Cameron Alvarado <cameronalvarado@Camerons-Macintosh.local>
2019-09-03 14:14:26 -0700

    Got response from API

--- react-github-card/src/App.js
+++ react-github-card/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import axios from 'axios';
 import CardList from "./components/CardList";
 
 class App extends React.Component  {
@@ -9,6 +10,15 @@ class App extends React.Component  {
     this.state = {}
   }
 
+  componentDidMount() {
+    const URL = 'https://api.github.com/users/CameronAlvarado/followers'
+    axios.get(URL)
+      .then(response => {
+        console.log(response.data)
+        response.json()})
+      .then(response => this.setState({}))
+      .catch(err => console.log('noooo'));
+  }
 
   render() {
     return (
