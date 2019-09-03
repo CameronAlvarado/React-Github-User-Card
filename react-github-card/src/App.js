716f9f34dab1788b9dc913160d54f1493c5d2e9a
Cameron Alvarado <cameronalvarado@Camerons-Macintosh.local>
2019-09-03 16:04:16 -0700

    Still working on getting user objects onto new setState array

--- react-github-card/src/App.js
+++ react-github-card/src/App.js
@@ -7,24 +7,39 @@ class App extends React.Component  {
 
   constructor() {
     super();
-    this.state = {}
+    this.state = {
+      users: [],
+      loginArray: []
+    }
+    console.log(this.state.loginArray)
   }
 
   componentDidMount() {
     const URL = 'https://api.github.com/users/CameronAlvarado/followers'
     axios.get(URL)
       .then(response => {
-        console.log(response.data)
-        response.json()})
-      .then(response => this.setState({}))
-      .catch(err => console.log('noooo'));
-  }
+        console.log(response.data);
+        this.setState({
+          users: response.data
+        })
+        console.log(this.state.users)
+      })
+      .catch(err => console.log('error'));
+      axios.get(`https://api.github.com/users/${this.state.users}`)
+      .then(response => {
+        this.setState({
+          loginArray: response.data
+        })
+      })
+      .catch(err => console.log("noooo"));
+    };
 
   render() {
     return (
       <div className="App">
         <h1>React GitHub Usercard</h1>
-        <CardList />
+        <CardList
+          userArray={this.state.users} />
       </div>
     );
   }
