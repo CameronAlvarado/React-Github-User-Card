e2ca583239a83fc1f6c6407e9ec7d4fd585a5877
Cameron Alvarado <cameronalvarado@Camerons-Macintosh.local>
2019-09-03 19:25:29 -0700

    Got sematic UI working. Cards are looking clean

--- react-github-card/src/components/CardList.js
+++ react-github-card/src/components/CardList.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import '../App.css';
+// import '../App.css';
 import { Container, Grid } from "semantic-ui-react";
+import 'semantic-ui-css/semantic.min.css';
 import UserCard from "./Card";
 
 class CardList extends React.Component  {
@@ -12,21 +13,21 @@ class CardList extends React.Component  {
 
 render() {
     return (
-      <div className="CardList">
-        <h2>Cardlist Component</h2>
-        <Container text>
-            <Grid relaxed columns={2} divided="vertically">
-                <Grid.Row padded="vertically" columns={3}>
-                    {this.props.userArray.map(user => {
-                            console.log(user);
-                            return (
-                            <UserCard userArray={user} />
-                            );
-                        })}
-                </Grid.Row>
-            </Grid>
-        </Container>
-      </div>
+        <section>
+            <h2>Cardlist Component</h2>
+            <Container text>
+                <Grid relaxed columns={2} divided="vertically">
+                    <Grid.Row padded="vertically" columns={2}>
+                        {this.props.userArray.map(user => {
+                                console.log(user);
+                                return (
+                                <UserCard userArray={user} />
+                                );
+                            })}
+                    </Grid.Row>
+                </Grid>
+            </Container>
+      </section>
     );
     }
   }
